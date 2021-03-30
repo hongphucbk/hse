b5b58cc85ef779173b143e8b70067ab6dc92e676
trieuppp01230 <trieunguyencd17tt10@gmail.com>
2021-03-30 21:08:14 +0700

    update ui

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,19 +24,6 @@ $('.login-signup-reset').find('input, textarea').on('keyup blur focus', function
             }
     }
 });
-  
-$('.tab-form-active a').on('click', function (e) {
-    e.preventDefault();
-    
-    $(this).parent().addClass('active');
-    $(this).parent().siblings().removeClass('active');
-    
-    target = $(this).attr('href');
-
-    $('.tab-content > div').not(target).hide();
-    
-    $(target).fadeIn(600);
-});
 
 $(".lession").click(function() {
     
